931c1e08302c4be0d5bfb3b0908f2fdfe133b95e
omar <medzhidov.omar11@gmail.com>
2026-05-27 21:34:13 +0000

    blah

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,10 +7,15 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
+      host: '0.0.0.0',
       port: parseInt(env.VITE_FRONTEND_PORT || '3005'),
+      strictPort: true,
+      hmr: {
+        clientPort: parseInt(env.VITE_FRONTEND_PORT || '3005')
+      },
       proxy: {
         '/api': {
-          target: `http://localhost:${env.VITE_BACKEND_PORT || '8099'}`,
+          target: `http://backend:${env.VITE_BACKEND_PORT || '8099'}`,
           changeOrigin: true
         }
       }
